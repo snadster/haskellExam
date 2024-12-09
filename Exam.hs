de727fb1081aa654d74789c46dfe6fa0bc5342c5
Sandra Johansen <sjoha23@student.sdu.dk>
2024-12-09 11:50:32 +0100

    finished arithmetic + added aestehtics

--- Exam.hs
+++ Exam.hs
@@ -3,7 +3,32 @@
 *        Author: Sandra K. Johansen        *
 *******************************************-}
 
--- TYPES
+--____________--
+-- GROUNDWORK --
+--____________--
+
+-- RUN PROGRAM --
+runProgram :: FilePath -> IO ()
+{- execute program, prints rv register on screen in case of termination -}
+runProgram file = interp (loadFromFile file)
+
+interp :: Program -> Int 
+{- Runs the entire program, returns value in register rv -}
+
+
+-- ACQUIRING DATA --
+loadFromFile :: FilePath -> IO Program
+{- 
+ Takes the register program file name and returns
+ IO action containing the program
+-}
+loadFromFile file = do content <- readFile file
+                       return (parsing content)
+
+
+--_________--
+--  TYPES  --
+--_________--
                     -- moving data
 data Instructions =   Mov  Reg Reg
                     | Movc Reg Int
@@ -19,14 +44,14 @@ data Instructions =   Mov  Reg Reg
                     -- jumping
                     | Jmp  Int
                     | Jal  Int
-                    | Jif  Reg Int
+                    | Jif  Int Reg
                     | Jr   Reg
     deriving (Read, Show)
 
 type Program = [Instructions]
 
 data Reg =    Rv        -- return value
-            | Lnk       -- keep index value
+            | Lnk       -- prior index value
             | R1
             | R2
             | R3
@@ -44,6 +69,7 @@ data Reg =    Rv        -- return value
             | R14
             | R15
             | R16
+            | Index     -- current line of program
     deriving (Read, Show)
 
 
@@ -51,56 +77,68 @@ data Reg =    Rv        -- return value
 -- FUNCTIONS --
 --___________--
 
-funcName :: Program -> NULL
--- need tp actually run through list of instructions,
--- so far we are NOT doing that. whoopsie.
-
-interp :: Program -> Int 
--- Runs the entire program, returns value in register rv
-
-
 -- DEFINITIONS --
 dataList :: [Int]
--- list of registers
-dataList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+{- list of registers -}
+dataList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-regToList :: Reg -> Int
--- index into dataList by:
--- dataList !! regToList R2
-regToList Rv  = 0
-regToList Lnk = 1
-regToList R1  = 2
-regToList R2  = 3
-regToList R3  = 4
-regToList R4  = 5
-regToList R5  = 6
-regToList R6  = 7
-regToList R7  = 8
-regToList R8  = 9
-regToList R9  = 10
-regToList R10 = 11
-regToList R11 = 12
-regToList R12 = 13
-regToList R13 = 14
-regToList R14 = 15
-regToList R15 = 16
-regToList R16 = 17
+index :: Reg -> Int
+{-
+ sets numbers for each Reg;
+ index into dataList by
+ dataList !! index R2
+-}
+index Rv    = 0
+index Lnk   = 1
+index R1    = 2
+index R2    = 3
+index R3    = 4
+index R4    = 5
+index R5    = 6
+index R6    = 7
+index R7    = 8
+index R8    = 9
+index R9    = 10
+index R10   = 11
+index R11   = 12
+index R12   = 13
+index R13   = 14
+index R14   = 15
+index R15   = 16
+index R16   = 17
+index Index = 18
 
+
+whoops :: IO ()
+{- raises error if attempt at non-existing memory -}
+whoops = error "Whoops! You're not allowed to go there."
+
+increment :: [a] -> [a]
+increment d = replace d (index Index, ((d !! index Index) + 1))
+
+
+-- INTERACTING WITH DATA --
+line :: [a] -> Int
+{- find current line of program -}
+line d = d !! index Index 
 
 parsing :: String -> Program
--- convert String from loadFile into a list of Instructions
+{- 
+ convert String from loadFile into a list of Instructions 
+ now the data is in this format:
+ info = ["Movc R4 R2", "Sub R3 R5"]
+-}
 parsing content = [read x | x <- info]
     where info  = lines content
-
--- now the data is in this format:
--- info = ["Movc R4 R2", "Sub R3 R5"]
--- with [read x | x <- info] it by itself can find and read the types
+-- with [read x | x <- info] haskell by itself can find and read the types
 
 
 replace ::[a] -> (Int, a) -> [a]
--- found the basis for this function online. 
--- needed something to change a register
--- takes list, index, and what it needs to put there.
+{- 
+ found the basis for this function online. 
+ needed something to change a register
+ takes list, index, and what it needs to put there.
+-} 
 replace [] _        = []
 replace (_:xs) (0,a) = a:xs
 replace (x:xs) (n,a) = x: replace xs (n-1, a)
@@ -108,10 +146,10 @@ replace (x:xs) (n,a) = x: replace xs (n-1, a)
 
 defInstruction :: Instructions -> [a] -> [a]
 {-
-calls the given function when it meets
-something it recognizes.
+ calls the given function when it meets
+ something it recognizes.
+ when using defInst. call it with dataList.
 -}
--- when using defInst. call it with dataList.
 
 -- move --
 defInstruction (Mov arg1 arg2)      = move (Mov arg1 arg2)
@@ -135,10 +173,10 @@ defInstruction (Jr arg1)            = jumping (Jr arg1)
 
 
 move :: Instructions -> [a] -> [a]
--- wherein d is the given list (dataList)
-move (Mov arg1 arg2)  d     = replace d (regToList arg2,
-                                      d !! (regToList arg1))
-move (Movc arg1 arg2) d     = replace d (regToList arg1, arg2)
+{- wherein d is the given list (dataList) -}
+move (Mov arg1 arg2)  d     = replace d (index arg2,
+                                      d !! (index arg1))
+move (Movc arg1 arg2) d     = replace d (index arg1, arg2)
 
 
 arithmetic :: Intructions -> [a] -> [a]
@@ -146,52 +184,47 @@ arithmetic :: Intructions -> [a] -> [a]
 instructions put results in the last listed register,
 i.e arg3 for Add, but arg1 for Addc.
 -}
-arithmetic (Add arg1 arg2 arg3) d = replace d (regToList arg3,
-                                           (d !! (regToList arg1) +
-                                           (d !! (regToList arg2))))
-arithmetic (Addc arg1 arg2)     d = replace d (regToList arg1,
-                                           (d !! (regToList arg1) + 
+arithmetic (Add arg1 arg2 arg3) d = replace d (index arg3,
+                                           (d !! (index arg1) +
+                                           (d !! (index arg2))))
+arithmetic (Addc arg1 arg2)     d = replace d (index arg1,
+                                           (d !! (index arg1) + 
                                             arg2))
 
-arithmetic (Sub arg1 arg2 arg3) d = replace d (regToList arg3,
-                                           (d !! (regToList arg1) -
-                                           (d !! (regToList arg2))))
-arithmetic (Subc arg1 arg2)     d = replace d (regToList arg1,
-                                           (d !! (regToList arg1) - 
+arithmetic (Sub arg1 arg2 arg3) d = replace d (index arg3,
+                                           (d !! (index arg1) -
+                                           (d !! (index arg2))))
+arithmetic (Subc arg1 arg2)     d = replace d (index arg1,
+                                           (d !! (index arg1) - 
                                             arg2))
 
-arithmetic (Mul arg1 arg2 arg3) d = replace d (regToList arg3,
-                                           (d !! (regToList arg1) *
-                                           (d !! (regToList arg2))))
-arithmetic (Mulc arg1 arg2)     d = replace d (regToList arg1,
-                                           (d !! (regToList arg1) * 
+arithmetic (Mul arg1 arg2 arg3) d = replace d (index arg3,
+                                           (d !! (index arg1) *
+                                           (d !! (index arg2))))
+arithmetic (Mulc arg1 arg2)     d = replace d (index arg1,
+                                           (d !! (index arg1) * 
                                             arg2))
 
-arithmetic (Div arg1 arg2 arg3) d = replace d (regToList arg3,
-                                           (d !! (regToList arg1) `div`
-                                           (d !! (regToList arg2))))
-arithmetic (Divc arg1 arg2)     d = replace d (regToList arg1,
-                                           (d !! (regToList arg1) `div` 
+arithmetic (Div arg1 arg2 arg3) d = replace d (index arg3,
+                                           (d !! (index arg1) `div`
+                                           (d !! (index arg2))))
+arithmetic (Divc arg1 arg2)     d = replace d (index arg1,
+                                           (d !! (index arg1) `div` 
                                             arg2))
 
 
 jumping :: Instructions -> [a] -> [a]
-jumping (Jmp arg1) d = info !! (regToList arg1)
-jumping (Jal arg1) d = info !! (regToList arg1) && replace d (1)
-jumping (Jif arg1) d = 
-jumping (Jr arg1)  d =
+jumping (Jmp arg1) d      = replace d (index Index, arg1)
 
+jumping (Jal arg1) d      = replace (replace d (index Lnk, 
+                                     (d !! index Index)))
+                                    (index Index, arg1)
 
-loadFromFile :: FilePath -> IO Program
--- Takes the register program file name and returns
--- IO action containing the program
--- i think it returns a program (a list of instructions)
+jumping (Jif arg1 arg2) d = if d !! index arg2 /= 0
+                                then jumping (Jmp arg1) d
+                                else increment d
 
-loadFromFile file = do content <- readFile file
-                       return (parsing content)
-
-
-runProgram :: FilePath -> IO ()
--- execute program, prints rv register on screen in case of termination.
-
-
+jumping (Jr arg1)  d      = if (d !! index arg1) > 1 &&
+                               (d !! index arg1) < 19
+                                then jumping (Jmp (d !! index arg1)) d
+                                else whoops  -- raise error
