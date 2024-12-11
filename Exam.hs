f399feb8f0ba7c743df5cae713f2857ff5eecd19
Sandra Johansen <sjoha23@student.sdu.dk>
2024-12-11 13:48:12 +0100

    deleted whoops, attempt at incrementing in run function

--- Exam.hs
+++ Exam.hs
@@ -15,10 +15,19 @@ runProgram file = interp (loadFromFile file)
 
 interp :: Program -> Int 
 {- Runs the entire program, returns value in register rv -}
-interp prog | prog == [] = d !! index Rv
-            | prog /= [] = defInstruction prog[Index]
-            | prog /= [] = increment dataList
+interp prog = runReg prog dataList
 
+
+runReg :: Program -> [Int] -> Int
+{- 
+execute each instruction 
+needs program to find line, dataList, returns integer
+-}
+runReg prog d = if prog == []
+                  then replace d (index Rv, runReg prog d) 
+              |   else let instruction = prog !! (d !! (index Index))
+                        in runReg prog (runInstruction instruction (increment d))
+              
 
 -- ACQUIRING DATA --
 loadFromFile :: FilePath -> IO Program
@@ -56,6 +65,7 @@ type Program = [Instructions]
 
 data Reg =    Rv        -- return value
             | Lnk       -- prior index value
+            | R0
             | R1
             | R2
             | R3
@@ -72,7 +82,6 @@ data Reg =    Rv        -- return value
             | R13
             | R14
             | R15
-            | R16
             | Index     -- current line of program
     deriving (Read, Show)
 
@@ -84,7 +93,7 @@ data Reg =    Rv        -- return value
 -- DEFINITIONS --
 dataList :: [Int]
 {- list of registers -}
-dataList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+dataList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 index :: Reg -> Int
 {-
@@ -94,35 +103,30 @@ index :: Reg -> Int
 -}
 index Rv    = 0
 index Lnk   = 1
-index R1    = 2
-index R2    = 3
-index R3    = 4
-index R4    = 5
-index R5    = 6
-index R6    = 7
-index R7    = 8
-index R8    = 9
-index R9    = 10
-index R10   = 11
-index R11   = 12
-index R12   = 13
-index R13   = 14
-index R14   = 15
-index R15   = 16
-index R16   = 17
+index R0    = 2
+index R1    = 3
+index R2    = 4
+index R3    = 5
+index R4    = 6
+index R5    = 7
+index R6    = 8
+index R7    = 9
+index R8    = 10
+index R9    = 11
+index R10   = 12
+index R11   = 13
+index R12   = 14
+index R13   = 15
+index R14   = 16
+index R15   = 17
 index Index = 18
 
-
-whoops :: IO ()
-{- raises error if attempt at non-existing memory -}
-whoops = error "Whoops! You're not allowed to go there.
-                The Rv register will be printed."
 
 increment :: [a] -> [a]
 increment d = replace d (index Index, ((d !! index Index) + 1))
 
 validInt :: Int -> Bool
-validInt arg | arg > 1 && arg < 19 = True
+validInt arg | arg > 2 && arg < 18 = True
              | otherwise           = False
 
 validReg :: Reg -> Bool
@@ -158,7 +162,7 @@ replace (_:xs) (0,a) = a:xs
 replace (x:xs) (n,a) = x: replace xs (n-1, a)
 
 
-defInstruction :: Instructions -> [a] -> [a]
+runInstruction :: Instructions -> [a] -> [a]
 {-
  calls the given function when it meets
  something it recognizes.
@@ -166,24 +170,24 @@ defInstruction :: Instructions -> [a] -> [a]
 -}
 
 -- move --
-defInstruction (Mov arg1 arg2)      = move (Mov arg1 arg2)
-defInstruction (Movc arg1 arg2)     = move (Movc arg1 arg2)
+runInstruction (Mov arg1 arg2)      = move (Mov arg1 arg2)
+runInstruction (Movc arg1 arg2)     = move (Movc arg1 arg2)
 
 -- arithmetic --
-defInstruction (Add arg1 arg2 arg3) = arithmetic (Add arg1 arg2 arg3)
-defInstruction (Addc arg1 arg2)     = arithmetic (Addc arg1 arg2)
-defInstruction (Sub arg1 arg2 arg3) = arithmetic (Sub arg1 arg2 arg3)
-defInstruction (Subc arg1 arg2)     = arithmetic (Subc arg1 arg2)
-defInstruction (Mul arg1 arg2 arg3) = arithmetic (Mul arg1 arg2 arg3)
-defInstruction (Mulc arg1 arg2)     = arithmetic (Mulc arg1 arg2)
-defInstruction (Div arg1 arg2 arg3) = arithmetic (Div arg1 arg2 arg3)
-defInstruction (Divc arg1 arg2)     = arithmetic (Divc arg1 arg2)
+runInstruction (Add arg1 arg2 arg3) = arithmetic (Add arg1 arg2 arg3)
+runInstruction (Addc arg1 arg2)     = arithmetic (Addc arg1 arg2)
+runInstruction (Sub arg1 arg2 arg3) = arithmetic (Sub arg1 arg2 arg3)
+runInstruction (Subc arg1 arg2)     = arithmetic (Subc arg1 arg2)
+runInstruction (Mul arg1 arg2 arg3) = arithmetic (Mul arg1 arg2 arg3)
+runInstruction (Mulc arg1 arg2)     = arithmetic (Mulc arg1 arg2)
+runInstruction (Div arg1 arg2 arg3) = arithmetic (Div arg1 arg2 arg3)
+runInstruction (Divc arg1 arg2)     = arithmetic (Divc arg1 arg2)
 
 -- jumping --
-defInstruction (Jmp arg1)           = jumping (Jmp arg1)
-defInstruction (Jal arg1)           = jumping (Jal arg1)
-defInstruction (Jif arg1)           = jumping (Jif arg1)
-defInstruction (Jr arg1)            = jumping (Jr arg1)
+runInstruction (Jmp arg1)           = jumping (Jmp arg1)
+runInstruction (Jal arg1)           = jumping (Jal arg1)
+runInstruction (Jif arg1)           = jumping (Jif arg1)
+runInstruction (Jr arg1)            = jumping (Jr arg1)
 
 
 move :: Instructions -> [a] -> [a]
@@ -231,23 +235,26 @@ arithmetic (Divc arg1 arg2)     d = replace d (index arg1,
 
 
 jumping :: Instructions -> [a] -> [a]
-jumping (Jmp arg1) d      = if validInt arg1
-                             then replace d (index Index, arg1)
-                            else whoops
+jumping (Jmp arg1) d      = replace d (index Index, arg1)
 
-jumping (Jal arg1) d      = if validInt arg1
-                             then replace (replace d (index Lnk, 
+jumping (Jal arg1) d      = replace (replace d (index Lnk, 
                                      (d !! index Index)))
                                      (index Index, arg1)
-                            else whoops
 
-jumping (Jif arg1 arg2) d = if validInt arg1 && validReg arg2
-                               && d !! index arg2 /= 0
+jumping (Jif arg1 arg2) d = if (d !! index arg2) /= 0
                              then jumping (Jmp arg1) d
-                            else if validInt arg1 || validReg arg2
-                             then whoops
-                            else increment d
+                             else d
 
-jumping (Jr arg1)  d      = if validReg arg1
-                                then jumping (Jmp (d !! index arg1)) d
-                                else whoops  -- raise error
+jumping (Jr arg1)  d      =  jumping (Jmp (d !! index arg1)) d
+
+
+--__________--
+-- ARCHIVE  --
+--__________--
+{-
+realized I can just let the program crash and burn if undefined behaviour.
+    whoops :: IO ()
+    {- raises error if attempt at non-existing memory -}
+    whoops = error "Whoops! You're not allowed to go there.
+                    The Rv register will be printed."
+-}
