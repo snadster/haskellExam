8fb442272ba9a65e9667409156f1a7cb331ca47f
Sandra Johansen <sjoha23@student.sdu.dk>
2024-12-19 18:52:00 +0100

    it works now

--- Exam.hs
+++ Exam.hs
@@ -20,8 +20,9 @@ module Exam where
 -- RUN PROGRAM --
 runProgram :: FilePath -> IO ()
 {- execute program, prints rv register on screen in case of termination -}
-runProgram file = interp (loadFromFile file)
--- needs to return Rv upon crash
+runProgram file = do program <- loadFromFile file
+                     print (interp program)
+-- needs to return Rv
 
 
 interp :: Program -> Int 
@@ -34,9 +35,9 @@ runReg :: Program -> [Int] -> Int
 execute each instruction 
 needs program to find line, dataList, returns integer
 -}
-runReg prog d = if prog == []
-                  then updateVal d (index Rv, runReg prog d) 
-              |   else let instruction = prog !! (d !! (index Index))
+runReg prog d = if length prog <= (d !! (index Index))
+                  then d !! (index Rv) 
+                  else let instruction = prog !! (d !! (index Index))
                         in runReg prog (runInstruction instruction (increment d))
               
 
@@ -87,7 +88,6 @@ data Reg =    Rv        -- return value
             | R10
             | R11
             | R12
-            | R12
             | R13
             | R14
             | R15
@@ -132,28 +132,13 @@ index R15   = 17
 index Index = 18
 
 
-increment :: [a] -> [a]
+increment :: [Int] -> [Int]
 {- inrements the Index counter -}
 increment d = updateVal d (index Index, 
                           ((d !! index Index) + 1))
 
 
-validInt :: Int -> Bool
-validInt arg | arg > 2 && arg < 18 = True
-             | otherwise           = False
-
-
-validReg :: Reg -> Bool
-validReg arg | (d !! index arg1) > 1 &&
-               (d !! index arg1) < 19   = True
-             | otherwise                = False
-
-
 -- INTERACTING WITH DATA --
-line :: [a] -> Int
-{- find current line of program -}
-line d = d !! index Index 
-
 parsing :: String -> Program
 {- 
  convert String from loadFile into a list of Instructions 
@@ -165,17 +150,17 @@ parsing content = [read x | x <- info]
 -- with [read x | x <- info] haskell by itself can find and read the types
 
 
-updateVal ::[a] -> (Int, a) -> [a]
+updateVal :: [a] -> (Int, a) -> [a]
 {- 
  change a register via indexing;
  takes: dataList (index, what to put there).
 -} 
 updateVal [] _        = []
 updateVal (_:xs) (0,a) = a:xs
-updateVal (x:xs) (n,a) = x: updateVal xs ((n-1), a)
+updateVal (x:xs) (n,a) = x:updateVal xs ((n-1), a)
 
 
-runInstruction :: Instructions -> [a] -> [a]
+runInstruction :: Instructions -> [Int] -> [Int]
 {-
  calls the given function when it meets
  something it recognizes.
@@ -199,11 +184,11 @@ runInstruction (Divc arg1 arg2)     = arithmetic (Divc arg1 arg2)
 -- jumping --
 runInstruction (Jmp arg1)           = jumping (Jmp arg1)
 runInstruction (Jal arg1)           = jumping (Jal arg1)
-runInstruction (Jif arg1)           = jumping (Jif arg1)
+runInstruction (Jif arg1 arg2)      = jumping (Jif arg1 arg2)
 runInstruction (Jr arg1)            = jumping (Jr arg1)
 
 
-move :: Instructions -> [a] -> [a]
+move :: Instructions -> [Int] -> [Int]
 {- 
  wherein d is the given list (dataList) 
  and the result is in the last listed register
@@ -213,7 +198,7 @@ move (Mov arg1 arg2)  d     = updateVal d (index arg2,
 move (Movc arg1 arg2) d     = updateVal d (index arg1, arg2)
 
 
-arithmetic :: Intructions -> [a] -> [a]
+arithmetic :: Instructions -> [Int] -> [Int]
 {-
  instructions put results in the last listed register,
  i.e arg3 for Add, but arg1 for Addc.
@@ -247,11 +232,11 @@ arithmetic (Divc arg1 arg2)     d = updateVal d (index arg1,
                                             arg2))
 
 
-jumping :: Instructions -> [a] -> [a]
+jumping :: Instructions -> [Int] -> [Int]
 jumping (Jmp arg1) d      = updateVal d (index Index, arg1)
 
 jumping (Jal arg1) d      = updateVal (updateVal d (index Lnk, 
-                                     (d !! index Index) - 1))
+                                                    d !! index Index))
                                      (index Index, arg1)
 
 jumping (Jif arg1 arg2) d = if (d !! index arg2) /= 0
--- TestSuite.hs
+++ TestSuite.hs
@@ -1,3 +1,86 @@
 module TestSuite where
 
 import Exam
+
+{-******************************************
+*   TestSuite.hs               05.01.25    *
+*        Author: Sandra K. Johansen        *
+*                                          *
+*   Requirements:                          *
+*   imported Exam to run functions         *
+*******************************************-}
+
+{-
+ Here, parts of the code itself gets tested,
+ using the smallest and simplest programs that I,
+ the author, could think of.
+-}
+
+{- testing moving data, returns 10 -}
+runMov :: Int
+runMov =  interp [Movc R0 10, Mov R0 Rv] 
+
+{- testing arithmetic, returns 20 -}
+runAdd :: Int
+runAdd =  interp [Movc R0 10, Movc R1 10, Add R0 R1 Rv]
+
+runAddc :: Int
+runAddc = interp [Addc R0 20, Mov R0 Rv]
+
+runSub :: Int
+runSub =  interp [Movc R0 30, Movc R1 10, Sub R0 R1 Rv]
+
+runSubc :: Int
+runSubc = interp [Addc R0 30, Subc R0 10, Mov R0 Rv]
+
+runMul :: Int
+runMul =  interp [Addc R0 2, Addc R1 10, Mul R0 R1 Rv]
+
+runMulc :: Int
+runMulc = interp [Addc R0 2, Mulc R0 10, Mov R0 Rv]
+
+runDiv :: Int
+runDiv =  interp [Addc R0 40, Addc R1 2, Div R0 R1 Rv]
+
+runDivc :: Int 
+runDivc = interp [Addc R0 40, Divc R0 2, Mov R0 Rv]
+
+
+{- testing jumps, returns 1 if true, otherwise 2 -}
+runJmp :: Int
+runJmp = interp [Addc R0 2, Addc R1 1, Jmp 4, 
+                 Mul R0 R1 R0, Sub R0 R1 R0, Mov R0 Rv]
+
+runJal :: Int
+runJal = interp [Movc R0 10, Jal 2, Mov Lnk Rv]
+
+runJif1 :: Int
+runJif1 = interp [Movc R0 1, Jif 3 R0, Addc R0 1, Mov R0 Rv]
+
+-- exception; this program loops forever upon failure
+runJif2 :: Int
+runJif2 = interp [Movc R0 0, Jif 0 R0, Movc Rv 1]
+
+runJr :: Int
+runJr = interp [Movc R0 3, Jr R0, Movc Rv 1, Addc Rv 1]
+
+
+{-
+         |
+        -+-
+         A
+        /=\               /\  /\    ___  _ __  _ __ __    __
+      i/ O \i            /  \/  \  / _ \| '__|| '__|\ \  / /
+      /=====\           / /\  /\ \|  __/| |   | |    \ \/ /
+      /  i  \           \ \ \/ / / \___/|_|   |_|     \  /
+    i/ O * O \i                                       / /
+    /=========\        __  __                        /_/    _
+    /  *   *  \        \ \/ /        /\  /\    __ _  ____  | |
+  i/ O   i   O \i       \  /   __   /  \/  \  / _` |/ ___\ |_|
+  /=============\       /  \  |__| / /\  /\ \| (_| |\___ \  _
+  /  O   i   O  \      /_/\_\      \ \ \/ / / \__,_|\____/ |_|
+i/ *   O   O   * \i
+/=================\
+       |___|
+
+-}
